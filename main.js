666ecae30319c8c6a815faf8a821c7490274b9ea
Maksym <maksym.tatarchuk@valtech.com>
2021-10-16 00:54:19 +0300

    First server part

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
-let system = require('./modules/system');
+var express = require ('express');
+var path = require('path');
+var system = require('./server/modules/system');
+var colors = require('colors');
 
+const PORT = process.env.PORT ?? 3000;
+const app = express();
 
-console.log(system.getCpuTemperature())
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, 'server', 'templates'));
+
+app.use(express.static(path.resolve(__dirname, 'static')));
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        'serverUpTime': system.getServerUpTime(),
+        'cpuTemperature': system.getCpuTemperature()
+    })
+})
+
+app.listen(PORT, () => console.log(colors.bold('Started')));
