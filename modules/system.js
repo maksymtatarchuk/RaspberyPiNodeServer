c32d4e7e4ba6a089618e2de95b8f7a13bebfe1df
Maksym <maksym.tatarchuk@valtech.com>
2021-10-15 23:42:58 +0300

    Temperature for Pi

--- modules/system.js
+++ modules/system.js
@@ -16,18 +16,12 @@ module.exports = {
 
     'getCpuTemperature': function() {
         try {
-            let isWindows = os.version();
-            if (isWindows) return 'Not available'
-        } catch(e) {
-            console.log('Windows ERROR: ', e)
-        }
-
-        try {
             let temp = fs.readFileSync("/sys/class/thermal/thermal_zone0/temp");
-            temp = Math.round(temp/1000) + 'C';
+            temp = Math.round(temp/1000);
             return temp
         } catch (e) {
-            console.log('Raspberry Pi  ERROR: ', e)
+            console.log('ERROR: Raspberry Pi only!')
+            return 0
         }
     }
 }
