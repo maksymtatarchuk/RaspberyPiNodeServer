d80a9a5e628c9acda56440e3487ba2165683bc01
Maksym <maksym.tatarchuk@valtech.com>
2021-10-18 22:53:06 +0300

    new sys servere feature err

--- server/utils/dataUtil.js
+++ server/utils/dataUtil.js
@@ -16,11 +16,12 @@ module.exports = {
         let d = Math.floor(seconds / 86400)
         let h = Math.floor(seconds / 3600 - d * 24)
         let m = Math.floor(seconds / 60 - d * 1440 - h * 60)
-        time.s = Math.floor(seconds - d * 86400 - h * 3600 - m * 60)
+        let s = Math.floor(seconds - d * 86400 - h * 3600 - m * 60)
 
         seconds / 86400 >= 1 ? time.d = d : '';
         seconds / 3600 >= 1 ? time.h = h : '';
         seconds / 60 >= 1 ? time.m = m : '';
+        time.s = s;
 
         time.viveData = numbToString(time)
 
