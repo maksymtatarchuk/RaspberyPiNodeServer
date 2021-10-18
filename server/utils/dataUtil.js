b45f6fced9057539e9fc4936dd904ffcceb860d1
Maksym <maksym.tatarchuk@valtech.com>
2021-10-18 22:39:05 +0300

    new sys servere feature err

--- server/utils/dataUtil.js
+++ server/utils/dataUtil.js
@@ -13,9 +13,9 @@ module.exports = {
     countTimer: function(seconds) {
         let time = {};
 
-        seconds >= 86400 ? time.d = Math.floor(seconds / 86400) : 0;
-        seconds >= 3600 ? time.h = Math.floor(seconds / 3600 - time.d * 24) : 0;
-        seconds >= 60 ? time.m = Math.floor(seconds / 60 - time.d * 1440 - time.h * 60) : 0;
+        time.d = seconds >= 86400 ? Math.floor(seconds / 86400) : 0;
+        time.h = seconds >= 3600 ? Math.floor(seconds / 3600 - time.d * 24) : 0;
+        time.m = seconds >= 60 ? Math.floor(seconds / 60 - time.d * 1440 - time.h * 60) : 0;
         time.s = Math.floor(seconds - time.d * 86400 - time.h * 3600 - time.m * 60);
 
         time.viveData = numbToString(time);
