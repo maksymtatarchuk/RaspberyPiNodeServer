43640b9907a6fef0c02ea453b4478515aa3ce558
Maksym <maksym.tatarchuk@valtech.com>
2021-10-18 22:37:31 +0300

    new sys servere feature err

--- server/utils/dataUtil.js
+++ server/utils/dataUtil.js
@@ -4,7 +4,7 @@ function numbToString(obj) {
     for (let key in obj) {
         result[key] = obj[key] < 10 & key !== 'd' ? '0' + obj[key] : obj[key].toString();
     }
-    console.log(obj)
+
     return result
 }
 
@@ -13,13 +13,13 @@ module.exports = {
     countTimer: function(seconds) {
         let time = {};
 
-        seconds >= 86400 ? time.d = Math.floor(seconds / 86400) : null;
-        seconds >= 3600 ? time.h = Math.floor(seconds / 3600 - time.d * 24) : null;
-        seconds >= 60 ? time.m = Math.floor(seconds / 60 - time.d * 1440 - time.h * 60) : null;
+        seconds >= 86400 ? time.d = Math.floor(seconds / 86400) : 0;
+        seconds >= 3600 ? time.h = Math.floor(seconds / 3600 - time.d * 24) : 0;
+        seconds >= 60 ? time.m = Math.floor(seconds / 60 - time.d * 1440 - time.h * 60) : 0;
         time.s = Math.floor(seconds - time.d * 86400 - time.h * 3600 - time.m * 60);
 
         time.viveData = numbToString(time);
-        console.log(time)
+
         return time
     }
 }
