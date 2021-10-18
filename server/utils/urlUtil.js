9efa80e1dcfb8e869f16784b6af13b440f55f139
Maksym <maksym.tatarchuk@valtech.com>
2021-10-19 02:04:21 +0300

    add shell executor update 0.004

--- server/utils/urlUtil.js
+++ server/utils/urlUtil.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const appDir = __dirname.split('\\server\\utils')[0]
+const appDir = __dirname.includes('\\') ? __dirname.split('\\server\\utils')[0] : __dirname.split('/server/utils')[0]
 
 const sysDir = path.resolve(appDir, 'sys')
 
