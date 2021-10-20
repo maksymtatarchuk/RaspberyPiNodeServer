78c8fd9ec44ea35b6be45456f88b29d39c083b07
Maksym <maksym.tatarchuk@valtech.com>
2021-10-21 00:55:30 +0300

    test

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -15,7 +15,7 @@ function updateSererDataMonitoringBar() {
             }
 
             // Update temperature
-            console.log(ata.temp.value)
+            console.log(data.temp.value)
             $('.js-server-temp').text(data.temp.value)
             let newTempClass = $('.js-server-temp').attr('class').split(' ').slice(0, 2).join(' ') + ' badge-' + data.temp.status;
             $('.js-server-temp').attr('class', newTempClass)
